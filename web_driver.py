5acc2176a07702d496c960bd2765c214d86ae4f7
Yusuf YILMAZ <109434872+yusufyilmazf@users.noreply.github.com>
2022-11-12 21:43:31 +0300

    Update web_driver.py

--- web_driver.py
+++ web_driver.py
@@ -1,1 +1,26 @@
+# Data Extraction from the website
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from warnings import filterwarnings
+filterwarnings("ignore")
+
+
+def get_data():
+    pass
+
+
+driver = webdriver.Chrome(
+    "####")
+driver.set_window_size(1250, 740)
+driver.set_window_position(0, 0)
+driver.get("####")
+
+input("Press Enter To Continue...")
+get_data()
+film_name_tag = driver.find_elements(By.TAG_NAME, 'h4')
+
+for k in film_name_tag:
+        print(k.text)
+        break
 
