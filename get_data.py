c4984467764eb8e59fab931460dd71e599a0a6db
Yusuf YILMAZ <109434872+yusufyilmazf@users.noreply.github.com>
2022-11-16 21:35:16 +0300

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -17,13 +17,26 @@ api = tweepy.API(auth)
 #search tweets
 tweets = api.search_tweets("####", tweet_mode="####")
 
-for tweet in tweets:
-    try:
-        print(tweet.retweeted_status.full_text)
-        print("=====")
-    except AttributeError:
-        print(tweet.full_text)
-        print("=====")
+
+results =  tweepy_api.search_tweets(q="Hashtag Name", lang="tr",count=3, result_type="recent")
+for tweet in results:
+    print("=====START=====")
+    print ("Number of Tweet  : ",tweet_number) 
+    print ("Twitter UserName : ",tweet.user.screen_name)
+    print ("=====================")
+    print ("Tweet Contets : ",tweet.text)
+    print ("=====================")
+    print ("Created Time : ",tweet.created_at)
+    print ("=====================")
+    print ("Like count : ",tweet.favorite_count)
+    print ("=====================")
+    print ("Retweet count : ",tweet.retweet_count)
+    print ("=====================")
+    print ("Tweeet Source  : ",tweet.source)
+    print ("=====================")
+    print ("Tweet Language : ",tweet.lang)
+    print ("=======END OF TWEET=======")
+    print ("  ")
 
 
         
